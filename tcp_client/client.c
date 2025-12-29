3131f277002665c4f9f43985b65325076985081f
TranThanhAn20225592 <An.TT225592@sis.hust.edu.vn>
2025-12-29 21:58:27 +0700

    update phần người chơi cố tình không trả lời

--- tcp_client/client.c
+++ tcp_client/client.c
@@ -5,6 +5,8 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <ctype.h>
+#include <sys/select.h>
+#include <sys/time.h>
 
 #define MAXLINE 4096
 
@@ -82,149 +84,155 @@ void menu(int logged_in) {
 void gamePlay(int sockfd, const char *my_username) {
     char recvBuff[MAXLINE];
     char sendBuff[MAXLINE];
-    int n;
-    int eliminated = 0;
+
     int skipLeft = -1;
+    int sentAnswer = 0;   // ? FLAG: có g?i ANSWER/SKIP hay chýa
 
     while (1) {
 
-        n = recv(sockfd, recvBuff, sizeof(recvBuff) - 1, 0);
-        if (n <= 0) {
-            puts("Disconnected from server!");
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(sockfd, &fds);
+        FD_SET(0, &fds);   // stdin
+
+        int maxfd = sockfd > 0 ? sockfd : 0;
+
+        if (select(maxfd + 1, &fds, NULL, NULL, NULL) < 0) {
+            perror("select");
             return;
         }
-        recvBuff[n] = 0;
 
-        // TACH NHIEU MESSAGE THEO \n 
-        char *saveptr;
-        char *line = strtok_r(recvBuff, "\n", &saveptr);
-
-        while (line != NULL) {
-            if (strncmp(line, "SKIP_INFO|", 10) == 0) {
-                char tmp[MAXLINE];
-                strncpy(tmp, line, sizeof(tmp));
-                strtok(tmp, "|");
-                char *countStr = strtok(NULL, "|");
-                if (countStr) {
-                    skipLeft = atoi(countStr);
-                }
+        /* ===================== SERVER MESSAGE ===================== */
+        if (FD_ISSET(sockfd, &fds)) {
+            int n = recv(sockfd, recvBuff, sizeof(recvBuff) - 1, 0);
+            if (n <= 0) {
+                puts("Disconnected from server!");
+                return;
             }
+            recvBuff[n] = 0;
 
-            // QUESTION 
-            if (strncmp(line, "QUES|", 5) == 0) {
+            char *saveptr;
+            char *line = strtok_r(recvBuff, "\n", &saveptr);
 
-                char tmp[MAXLINE];
-                strncpy(tmp, line, sizeof(tmp));
-                tmp[sizeof(tmp) - 1] = 0;
+            while (line) {
 
-                strtok(tmp, "|");
-                char *question = strtok(NULL, "|");
-                char *op1 = strtok(NULL, "|");
-                char *op2 = strtok(NULL, "|");
-                char *op3 = strtok(NULL, "|");
-                char *op4 = strtok(NULL, "|");
-
-                printf("\n[QUESTION]: %s\n", question);
-                printf("1. %s\n", op1);
-                printf("2. %s\n", op2);
-                printf("3. %s\n", op3);
-                printf("4. %s\n", op4);
-
-                if (skipLeft >= 0) {
-                    printf("\n------------------------------------------------\n");
-                    if (skipLeft > 0) {
-                        printf("(!) You have %d skips left (enter 5 to skip)\n", skipLeft);
-                    } else {
-                        printf("(!) You have no skips left!\n");
-                        printf("    (If you enter 5 now, you will be eliminated)\n");
-                    }
-                    printf("------------------------------------------------\n");    
+                /* ---------- SKIP INFO ---------- */
+                if (strncmp(line, "SKIP_INFO|", 10) == 0) {
+                    char tmp[MAXLINE];
+                    strncpy(tmp, line, sizeof(tmp));
+                    strtok(tmp, "|");
+                    char *countStr = strtok(NULL, "|");
+                    if (countStr) skipLeft = atoi(countStr);
                 }
 
-                int ans = 0;
-                do {
+                /* ---------- QUESTION ---------- */
+                else if (strncmp(line, "QUES|", 5) == 0) {
+
+                    sentAnswer = 0;   // ? reset cho câu m?i
+
+                    char tmp[MAXLINE];
+                    strncpy(tmp, line, sizeof(tmp));
+                    tmp[sizeof(tmp) - 1] = 0;
+
+                    strtok(tmp, "|");
+                    char *question = strtok(NULL, "|");
+                    char *op1 = strtok(NULL, "|");
+                    char *op2 = strtok(NULL, "|");
+                    char *op3 = strtok(NULL, "|");
+                    char *op4 = strtok(NULL, "|");
+
+                    printf("\n[QUESTION]: %s\n", question);
+                    printf("1. %s\n", op1);
+                    printf("2. %s\n", op2);
+                    printf("3. %s\n", op3);
+                    printf("4. %s\n", op4);
+
+                    if (skipLeft >= 0) {
+                        printf("\n---------------------------------------------\n");
+                        if (skipLeft > 0)
+                            printf("(!) You have %d skips left (enter 5 to skip)\n", skipLeft);
+                        else
+                            printf("(!) No skips left (5 = eliminated)\n");
+                        printf("---------------------------------------------\n");
+                    }
+
                     printf("Enter your answer (1-4");
                     if (skipLeft >= 0) printf(", 5=SKIP");
                     printf("): ");
+                    fflush(stdout);
+                }
 
-                    if (scanf("%d", &ans) != 1) {
-                        int c;
-                        while ((c = getchar()) != '\n' && c != EOF) {}
-                        ans = 0;
+                /* ---------- STAGE 1 RESULT ---------- */
+                else if (strncmp(line, "RESULT|", 7) == 0) {
+                    char tmp[MAXLINE];
+                    strncpy(tmp, line, sizeof(tmp));
+                    strtok(tmp, "|");
+                    char *winner = strtok(NULL, "|");
+
+                    if (winner && strcmp(winner, "NONE") == 0)
+                        puts("No winner for this question.");
+                    else if (winner && strcmp(winner, my_username) == 0)
+                        puts("You are the MAIN player!");
+                    else
+                        puts("You are a SUB player.");
+                }
+
+                /* ---------- SCORE ---------- */
+                else if (strncmp(line, "SCORE|YOU|", 10) == 0) {
+                    strtok(line, "|");
+                    strtok(NULL, "|");
+                    char *score = strtok(NULL, "|");
+                    printf("Your score: %s\n", score);
+                }
+
+                /* ---------- LOG ---------- */
+                else if (strncmp(line, "LOG|", 4) == 0) {
+                    printf("[SPECTATOR] %s\n", line + 4);
+                }
+
+                /* ---------- RESULT / ACK CODES ---------- */
+                else if (isdigit((unsigned char)line[0])) {
+
+                    /* ? CH? IN 300 N?U NGÝ?I CHÕI Ð? G?I ANSWER */
+                    if (strcmp(line, "300") == 0) {
+                        if (sentAnswer) {
+                            explain_code(line);   // Answer received
+                        }
+                        sentAnswer = 0;  // reset sau ACK
                     }
-                } while (ans < 1 || ans > 5);
+                    else {
+                        explain_code(line);
+                    }
 
-                int c;
-                while ((c = getchar()) != '\n' && c != EOF) {}
+                    if (strcmp(line, "410") == 0) {
+                        puts("You are eliminated. Watching the game...");
+                    }
+                    else if (strcmp(line, "420") == 0 ||
+                             strcmp(line, "421") == 0) {
+                        return;
+                    }
+                }
+
+                line = strtok_r(NULL, "\n", &saveptr);
+            }
+        }
+
+        /* ===================== USER INPUT ===================== */
+        if (FD_ISSET(0, &fds)) {
+            int ans;
+            if (scanf("%d", &ans) == 1) {
+                while (getchar() != '\n');
 
                 if (ans == 5) {
-                    snprintf(sendBuff, sizeof(sendBuff), "SKIP\n");
-                    printf("Sent SKIP...\n");
+                    send(sockfd, "SKIP\n", 5, 0);
                 } else {
-                    snprintf(sendBuff, sizeof(sendBuff), "ANSWER %d\n", ans);
-                    printf("Sent ANSWER %d...\n", ans);
+                    snprintf(sendBuff, sizeof(sendBuff),
+                             "ANSWER %d\n", ans);
+                    send(sockfd, sendBuff, strlen(sendBuff), 0);
                 }
 
-                send(sockfd, sendBuff, strlen(sendBuff), 0);
-
-                skipLeft = -1; // reset skip info
+                sentAnswer = 1;   // ? ðánh d?u ð? g?i
             }
-
-            //CHON MAIN (STAGE 1)
-            else if (strncmp(line, "RESULT|", 7) == 0) {
-
-                char tmp[MAXLINE];
-                strncpy(tmp, line, sizeof(tmp));
-                tmp[sizeof(tmp) - 1] = 0;
-
-                strtok(tmp, "|");
-                char *winner = strtok(NULL, "|");
-
-                if (winner && strcmp(winner, "NONE") == 0) {
-                    puts("No winner for this question.");
-                }
-                else if (winner && strcmp(winner, my_username) == 0) {
-                    puts("You are the MAIN player!");
-                }
-                else {
-                    puts("You are a SUB player.");
-                }
-            }
-
-            // SCORE
-            else if (strncmp(line, "SCORE|YOU|", 10) == 0) {
-                strtok(line, "|"); // SCORE
-                strtok(NULL, "|"); // YOU
-                char *score = strtok(NULL, "|");
-                printf("Your score: %s\n", score);
-            }
-            
-            else if (strncmp(line, "LOG|", 4) == 0) {
-                printf("[SPECTATOR] %s\n", line + 4);
-            }
-
-            // MAIN ROUND RESULT (CODE)
-            else {
-                if (isdigit((unsigned char)line[0])) {
-                    explain_code(line);
-                    if (strcmp(line, "410") == 0) {
-                       eliminated = 1;
-                       //return;   // quay ve menu
-                       puts("You are eliminated. Watching the game...");
-                    } else if (strcmp(line, "420") == 0) {                       
-                        return;
-                    } else if (strcmp(line, "421") == 0) {
-                        puts("Game Over. Returning to Menu...");
-                        return;
-                    }
-                }
-                // message khac
-                else if (strcmp(line, "300") != 0) {
-                    printf("[SERVER]: %s\n", line);
-                }
-            }
-
-            line = strtok_r(NULL, "\n", &saveptr);
         }
     }
 }
